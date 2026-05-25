dae06e352e3936d01b9a4b406d6e1de518976bb0
kapil-0455 <kapil0455cu@gmail.com>
2026-05-25 15:57:07 +0530

    Making a script, and jsx intro

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-]);
-
-console.log(parent); // object
+// JSX -> react.createElement -> ReactElement(object) -> HTML DOM
+const heading = (
+    <h1 className="head">Hello World</h1>
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+root.render(heading);
