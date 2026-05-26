709ee363ac32ca6b93f96e961999dec87d81d60a
kapil-0455 <kapil0455cu@gmail.com>
2026-05-26 11:35:10 +0530

    making functional components

--- App.js
+++ App.js
@@ -6,5 +6,25 @@ const heading = (
     <h1 className="head">Hello World</h1>
 )
 
+// creating react components ->
+// functional component 
+
+const Title = () =>{
+    <h1>Title</h1>
+}
+
+// this is known as Compnent composition 
+// using one component inside another component -> 
+const HeadingComponent = () =>{
+    return (
+        <div> 
+            <Title/> 
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+// we have to render like this 
+root.render(<HeadingComponent />);
