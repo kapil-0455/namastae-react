ee9b579da10d3c50b16701905dc0de3d171a7c60
kapil-0455 <kapil0455cu@gmail.com>
2026-06-15 17:24:50 +0530

    making appRouter from react router dom

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+const About = ()=>{
+    return (
+        <div>
+            <h1>About us </h1>
+            <h3>What you want to Know</h3>
+        </div>
+    )
+}
+
+export default About
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,62 +1,91 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
- 
+
 import ResCard from "./Restaurant";
-import Shimmer from "./Shimmer"
+import Shimmer from "./Shimmer";
 
 const Body = () => {
+  const [listRestaurants, setListRestaurants] = useState([]);
+  const [filterdData, setFilterdData] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
-    const [listRestaurants , setListRestaurants] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        fetchData();
-    } , []);
+  const fetchData = async () => {
+    try {
+      const data = await fetch(
+        "https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001",
+      );
 
-    const fetchData = async () => {
-        try {
-            const data = await fetch(
-                "https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001"
-            );
+      const json = await data.json();
+      console.log("Fetched JSON:", json);
 
-            const json = await data.json();
-            console.log("Fetched JSON:", json);
+      // Optional chaining to find the correct restaurant card
+      const restaurants =
+        json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants ||
+        json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants ||
+        [];
 
-            // Optional chaining to find the correct restaurant card
-            const restaurants =
-                json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants ||
-                json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants ||
-                [];
-
-            setListRestaurants(restaurants);
-        } catch (error) {
-            console.error("Error fetching data:", error);
-        }
-    };
-
-    if(listRestaurants.length === 0){
-        return <Shimmer></Shimmer>
+      setListRestaurants(restaurants);
+      setFilterdData(restaurants);
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
-    return (
-        <div className="body">
-            <div className="search">
-                <button onClick={() => {
-                    console.log("Button Clicked!");
-                    const filteredList = listRestaurants.filter(
-                        (res) => res.info.avgRating > 4.3
-                    );
-                    console.log("Filtered List:", filteredList);
-                    setListRestaurants(filteredList);
-                }}>
-                    Tap to search top Rated
-                </button>
-            </div>
-            <div className="res-container">
-                {listRestaurants.map((restaurant) => {
-                     return <ResCard key={restaurant.info.id} resData={restaurant}/>;
-                })}
-            </div>
+  };
+
+  if (listRestaurants.length === 0) {
+    return <Shimmer></Shimmer>;
+  }
+  return (
+    <div className="body">
+      <div className="filter-container">
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="Search restaurants..."
+            value={searchText}
+            className="search-input"
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+
+          <button
+            className="btn-search"
+            onClick={() => {
+              const filterData = listRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase()),
+              );
+
+              setFilterdData(filterData);
+            }}
+          >
+            Search
+          </button>
         </div>
-    );
+        <button
+          className="btn-filter"
+          onClick={() => {
+            console.log("Button Clicked!");
+            const filteredList = listRestaurants.filter(
+              (res) => res.info.avgRating > 4.3,
+            );
+            console.log("Filtered List:", filteredList);
+            setFilterdData(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {filterdData.map((restaurant) => {
+          return <ResCard key={restaurant.info.id} resData={restaurant} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Body;
