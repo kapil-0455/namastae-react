873d745942cd4f0d55be9179b9d8abc996447d3f
kapil-0455 <kapil0455cu@gmail.com>
2026-06-27 19:37:58 +0530

    Making state variables in class components.

--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -4,15 +4,21 @@ class UserClass extends React.Component{
     constructor(props){
         super(props);
 
-        
+        this.state = {
+            count : 0 , 
+            count2 : 1,
+        }
     }
     render(){
 
         const {name , branch } = this.props;
+        const {count , count2 } = this.state;
         <div>
             <h2>Name : {name}</h2>
             <h3>Branch : {branch} </h3>
             <h4>Job Role : Software Developer</h4>
+            <h2>Count : {count} </h2>
+            <h2>Count2 : {count2} </h2>
         </div>
 
     }
