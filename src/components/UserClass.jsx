e6cf5fb730994192e0b8d203bfd52b44d14617f3
kapil-0455 <kapil0455cu@gmail.com>
2026-06-27 19:53:25 +0530

    how to set state variables

--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -1,27 +1,33 @@
-import React from "react"
+import React from "react";
 
-class UserClass extends React.Component{
-    constructor(props){
-        super(props);
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            count : 0 , 
-            count2 : 1,
-        }
-    }
-    render(){
+    this.state = {
+      count: 0,
+      count2: 1,
+    };
+  }
+  render() {
+    const { name, branch } = this.props;
+    const { count, count2 } = this.state;
 
-        const {name , branch } = this.props;
-        const {count , count2 } = this.state;
-        <div>
-            <h2>Name : {name}</h2>
-            <h3>Branch : {branch} </h3>
-            <h4>Job Role : Software Developer</h4>
-            <h2>Count : {count} </h2>
-            <h2>Count2 : {count2} </h2>
-        </div>
-
-    }
+    <div>
+      <h2>Name : {name}</h2>
+      <h3>Branch : {branch} </h3>
+      <button
+        onClick={() => {
+          this.setState({
+            count: this.state.count +1,
+          });
+        }}
+      ></button>
+      <h4>Job Role : Software Developer</h4>
+      <h2>Count : {count} </h2>
+      <h2>Count2 : {count2} </h2>
+    </div>;
+  }
 }
 
-export default UserClass
+export default UserClass;
